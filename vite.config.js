d2d8851184c10be0117579efd53f4b171ea19b6f
Pedromallcher <pedromallcher@gmail.com>
2025-01-19 13:35:00 -0300

    feat(change vite config)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-portfolio-template/",
+  // base: "/react-portfolio-template/",
+  base: "/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
